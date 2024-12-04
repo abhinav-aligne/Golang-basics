24a8d4b469e342aa754bd91d591fbf7081c218cd
abhinav-aligne <abhinav.gera@aligne.ai>
2024-12-04 16:22:41 +0530

    2. data types variables intializing

--- main.go
+++ main.go
@@ -3,5 +3,11 @@ package main
 import "fmt"
 
 func main() {
-	fmt.Println("Hello world!")
+	// initialize variables here
+	var smsSendingLimit int
+	var costPerSMS float64
+	var hasPermission bool
+	var username string
+	
+	fmt.Printf("%v %.2f %v %q\n", smsSendingLimit, costPerSMS, hasPermission, username)
 }
