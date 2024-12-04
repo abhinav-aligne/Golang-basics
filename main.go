cccab167689d5715f0e0ee5cbde794a153114bb2
abhinav-aligne <abhinav.gera@aligne.ai>
2024-12-04 16:27:31 +0530

    assignment-1 completed

--- main.go
+++ main.go
@@ -1,13 +1,18 @@
+// 										ASSIGNMENT
+// A common use case for Textio is to send birthday messages.
+// Complete the main function. It should print: "Happy birthday! You are now 21 years old!".
+// Create a string variable messageStart with the text "Happy birthday! You are now"
+// Create an integer variable age set to 21
+// Create another string variable messageEnd with the text "years old!"
+// The provided fmt.Println statement will print the full message on a single line separated by spaces.
+
 package main
 
 import "fmt"
 
 func main() {
-	// initialize variables here
-	var smsSendingLimit int
-	var costPerSMS float64
-	var hasPermission bool
-	var username string
-	
-	fmt.Printf("%v %.2f %v %q\n", smsSendingLimit, costPerSMS, hasPermission, username)
+	messageStart := "Happy birthday! You are now"
+	age := 21
+	messageEnd := "years old!"
+	fmt.Println(messageStart, age, messageEnd)
 }
